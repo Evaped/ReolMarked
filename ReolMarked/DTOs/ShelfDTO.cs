025f2a4ddd95948a41375659505abba34f31e632
Eva Pedersen <eva.pedersen90@gmail.com>
2023-09-21 18:40:35 +0200

    oprettet ShelfDTO

---
+++ ReolMarked/DTOs/ShelfDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReolMarked.DTOs;
+public class ShelfDTO
+{
+    public string Location { get; set; }
+    public ShelfType ShelfType { get; set; }
+}

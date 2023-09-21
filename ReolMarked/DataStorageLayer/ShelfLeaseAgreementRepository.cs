8f4b90b7196e5719bbc6c22ac25f82100ebe2915
Eva Pedersen <eva.pedersen90@gmail.com>
2023-09-21 17:53:21 +0200

    oprettet ShelfLeaseAgreementRepository

---
+++ ReolMarked/DataStorageLayer/ShelfLeaseAgreementRepository.cs
@@ -0,0 +1,15 @@
+﻿using ReolMarked.DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReolMarked.DataStorageLayer;
+public class ShelfLeaseAgreementRepository : GenericRepository<ShelfLeaseAgreement>
+{
+    public ShelfLeaseAgreementRepository(DataBaseContext dbContext) : base(dbContext)
+    {
+
+    }
+}

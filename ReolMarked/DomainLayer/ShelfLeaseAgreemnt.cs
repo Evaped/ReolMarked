7836dda7b96fc232acab836b5409b0701d730eae
Abdurahmon K <abbmuslim@gmail.com>
2023-09-21 14:02:37 +0200

    Add Shelf and Renter properties to ShelfLeaseAgreement class.

--- ReolMarked/DomainLayer/ShelfLeaseAgreemnt.cs
+++ ReolMarked/DomainLayer/ShelfLeaseAgreemnt.cs
@@ -2,5 +2,10 @@ namespace ReolMarked.DomainLayer;
 
 public class ShelfLeaseAgreement
 {
+    public Shelf Shelf { get; set; }
+    public int ShelfId { get; set; }
+
+    public Renter Renter { get; set; }
+    public int RenterId { get; set; }
     
 }

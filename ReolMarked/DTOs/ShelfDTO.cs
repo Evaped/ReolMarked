a89ebe188fba1d1e3044726b8cecde381ebb5c67
Eva Pedersen <eva.pedersen90@gmail.com>
2023-09-21 19:15:37 +0200

    opdateret ShelfDTO

--- ReolMarked/DTOs/ShelfDTO.cs
+++ ReolMarked/DTOs/ShelfDTO.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ReolMarked.DTOs;
 public class ShelfDTO
 {
     public string Location { get; set; }
-    public ShelfType ShelfType { get; set; }
+    public ShelfType _shelfType { get; set; }
+
+    public ShelfDTO MapShelfToDTO(Shelf shelf)
+    {
+        return new ShelfDTO
+        {
+            Location = shelf.Location,
+            _shelfType = (ShelfType)shelf.shelfType
+        };
+    }
 }

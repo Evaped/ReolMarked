14b805a13d56d67e0b087793728306319bf4249a
BenjaminBDC <bdch54128@edu.ucl.dk>
2023-09-25 13:41:27 +0200

    lavet tests til lease agreement

--- tests/LeaseAgreementRepositoryShould.cs
+++ tests/LeaseAgreementRepositoryShould.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ReolMarked;
 using ReolMarked.DataStorageLayer;
+using ReolMarked.DomainLayer;
+using ReolMarked.DTOs;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,7 +27,49 @@ namespace tests
         }
 
         [Fact]
-        public async Task
+        public async Task AddLeaseAgreement()
+        {
+            var repoRenter = new RenterRepository(_dbContext);
+            var repo = new LeaseAgreementRepository(_dbContext);
+            var LeaseDTO = new LeaseAgreementDTO()
+            {
+                StartDate = DateTime.Now,
+                RentDuration = 10,
+                Price = 500,
+                ShelvesCount = 1,
+                Email = "email@benni.com",
+            };
+            
+            Renter renter = await repoRenter.GetByEmailAsync(LeaseDTO.Email);
+
+            if (renter == null)
+            {
+                renter = new Renter()
+                {
+                    Email = LeaseDTO.Email
+                };
+                await repoRenter.CreateAsync(renter);
+            }
+            
+            var leaseAgreement = new LeaseAgreement()
+            {
+                StartDate = LeaseDTO.StartDate,
+                RentDuration = LeaseDTO.RentDuration,
+                Price = LeaseDTO.Price,
+                ShelvesCount = LeaseDTO.ShelvesCount,
+                RenterId = renter.Id
+            };
+            await repo.CreateAsync(leaseAgreement);
+
+            var leases = await repo.GetAsync();
+            Assert.Single(leases); 
+        }
+
+        //[Fact]
+        //public Async Task UpdateLeaseAgreement()
+        //{
+
+        //}
         
     }
 }

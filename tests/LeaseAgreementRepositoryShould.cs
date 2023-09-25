e59d68011a16208d3173f173ada2b5c21fc8083c
BenjaminBDC <bdch54128@edu.ucl.dk>
2023-09-25 12:26:43 +0200

    laver flere test

---
+++ tests/LeaseAgreementRepositoryShould.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReolMarked.DataStorageLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tests
+{
+    
+    public class LeaseAgreementRepositoryShould
+    {
+        private readonly SqliteDbContext _dbContext;
+
+        public LeaseAgreementRepositoryShould()
+        {
+            var dbOptions = new DbContextOptionsBuilder().UseInMemoryDatabase(
+            Guid.NewGuid().ToString());
+
+            _dbContext = new SqliteDbContext(dbOptions.Options);
+        }
+
+        [Fact]
+        public async Task
+        
+    }
+}

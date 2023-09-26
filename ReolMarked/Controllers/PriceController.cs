0fc270604991c78cf4d9a9aebaeeacd80e541d31
Eva Pedersen <eva.pedersen90@gmail.com>
2023-09-26 15:00:01 +0200

    oprettet PriceControllor

--- ReolMarked/Controllers/PriceController.cs
+++ ReolMarked/Controllers/PriceController.cs
@@ -1,4 +1,6 @@
-﻿using ReolMarked.DTOs;
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
+using ReolMarked.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,39 @@ using System.Threading.Tasks;
 
 namespace ReolMarked.Controllers;
 
-public class PriceController : PriceDto
+[Route("api/[controller]")]
+[ApiController]
+public class PriceController : ControllerBase
 {
+    // POST: api/Shelf
+    [HttpPost]
+    public async Task<PriceDto> GetPrice(int shelfCount, int weeksCount, ShelfType shelftype)
+    {
+        var priceDto = new PriceDto
+        {
+            Price = 0,
+            Discount = 0
+        };
+        double discountPercent;
+
+        if (weeksCount >= 4 && weeksCount < 8)
+        {
+            discountPercent = 0.875;
+        }
+        else if (weeksCount >= 8)
+        {
+            discountPercent = 0.75;
+        }
+
+        else
+        {
+            discountPercent = 1;
+        }
+
+        priceDto.Price = (shelfCount * 100 * weeksCount) * discountPercent;
+        priceDto.Discount = (shelfCount * 100 * weeksCount) - priceDto.Price;
+
+        return priceDto;
+    }
 }
 
